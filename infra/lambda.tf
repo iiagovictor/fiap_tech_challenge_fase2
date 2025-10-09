b099afc07d86b70456cdd4b2bf63919ff188d1d8
iiagovictor <contato@iagovictor.com.br>
2025-10-08 23:51:15 -0300

    :sparkles: feat: lambda.tf

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,35 +1,10 @@
-resource "null_resource" "install_lambda_dependencies" {
-  triggers = {
-    requirements = fileexists("${path.module}/../app/lambda/requirements.txt") ? filemd5("${path.module}/../app/lambda/requirements.txt") : ""
-    source_code  = sha256(join("", [for f in fileset("${path.module}/../app/lambda", "*.py") : filemd5("${path.module}/../app/lambda/${f}")]))
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      cd ${path.module}/../app/lambda
-      if [ -f requirements.txt ]; then
-        pip install -r requirements.txt -t . --upgrade
-      fi
-    EOT
-  }
-}
-
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../app/lambda"
-  output_path = "${path.module}/../app/lambda/main.zip"
-  excludes    = ["main.zip", "__pycache__", "*.pyc", "*.dist-info", "*.egg-info"]
-  
-  depends_on = [null_resource.install_lambda_dependencies]
-}
-
 resource "aws_lambda_function" "lambda_trigger" {
   function_name    = "${var.project_name}_lambda_trigger"
   role             = aws_iam_role.lambda_role.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.9"
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = "${path.module}/../app/lambda/main.zip"
+  source_code_hash = filebase64sha256("${path.module}/../app/lambda/main.zip")
   timeout          = 30
 
   environment {
